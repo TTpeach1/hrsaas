5d4fb26756f0c14659207d213fad6732821a8c87
TTpeach1 <TTpeach1@Outlook.com>
2022-08-02 14:37:55 +0800

    页面路由全局守卫配置

---
+++ src/permission.js
@@ -0,0 +1,34 @@
+import router from '@/router'
+import store from '@/store'
+
+//路由（全局）前置守卫
+//会在所有路由进去之前触发
+//to:去哪里的路由信息
+//from：来自于哪个路由的信息
+//next：是否进入
+const whiteList = ['/login', '/404']
+router.beforeEach((to, from, next) => {
+  const token = store.state.user.token
+  //1.登录
+  if (token) {
+    // 是否进入登录页
+    if (to.path === '/login') {
+      // 1.1 是跳到首页
+      next('/')
+    } else {
+      // 1.2不是直接进入
+      next()
+    }
+  } else {
+    // 2.未登陆
+    const isCludes = whiteList.includes(to.path)
+    // 访问的是否在白名单(未登录也能访向的页面
+    if (isCludes) {
+      // 2.1在白名单 放行
+      next()
+    } else {
+      // 2.2不在白名单(不登录不能访问） 跳到登录页
+      next('/login')
+    }
+  }
+})
--- src/router/index.js
+++ src/router/index.js
@@ -47,12 +47,14 @@ export const constantRoutes = [
     path: '/',
     component: Layout,
     redirect: '/dashboard',
-    children: [{
-      path: 'dashboard',
-      name: 'Dashboard',
-      component: () => import('@/views/dashboard/index'),
-      meta: { title: 'Dashboard', icon: 'dashboard' }
-    }]
+    children: [
+      {
+        path: 'dashboard',
+        name: 'Dashboard',
+        component: () => import('@/views/dashboard/index'),
+        meta: { title: 'Dashboard', icon: 'dashboard' }
+      }
+    ]
   },
 
   {
@@ -120,13 +122,15 @@ export const constantRoutes = [
             children: [
               {
                 path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
+                component: () =>
+                  import('@/views/nested/menu1/menu1-2/menu1-2-1'),
                 name: 'Menu1-2-1',
                 meta: { title: 'Menu1-2-1' }
               },
               {
                 path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
+                component: () =>
+                  import('@/views/nested/menu1/menu1-2/menu1-2-2'),
                 name: 'Menu1-2-2',
                 meta: { title: 'Menu1-2-2' }
               }
@@ -164,13 +168,28 @@ export const constantRoutes = [
   { path: '*', redirect: '/404', hidden: true }
 ]
 
-const createRouter = () => new Router({
-  // mode: 'history', // require service support
-  scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
-})
+const createRouter = () =>
+  new Router({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes
+  })
 
 const router = createRouter()
+
+// //路由（全局）前置守卫
+// //会在所有路由进去之前触发
+// router.beforeEach((to, from, next) => {
+//   console.log(to)
+//   //进行权限控制
+//   //调用next 进入该路由，如果没有调用则无法进入
+//   if(to.path==='/login'){
+//     next()
+//   }else{
+//     next('/login')
+//   }
+// })
+//(放到permission)
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
